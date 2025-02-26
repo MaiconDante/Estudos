5b3303ff920af4736fafb3a836e1b8008b1fd689
Maicon P. B. Birolo <maicondante@hotmail.com>
2025-02-26 02:05:35 -0300

    Novas Funcionalidades e Datetime

--- Meus_Scripts/DIO/PythonDeveloper/sistema_bancario.py
+++ Meus_Scripts/DIO/PythonDeveloper/sistema_bancario.py
@@ -1,13 +1,25 @@
+from datetime import datetime
+
 saldo = 0
 limite = 500
 saques_realizados = 0
-LIMITE_SAQUES = 3
+depositos_realizados = 0
 total_depositos = 0
 total_saques = 0
-depositos_realizados = 0
+LIMITE_SAQUES = 3
+TRANSACOES_LIMITE = 10
+transacoes_realizadas = []
+data_atual = datetime.today()
 
+def registrar_transacao(valor, natureza):
+    global data_atual, TRANSACOES_LIMITE, transacoes_realizadas
+    transacoes_hoje = [t for t in transacoes_realizadas if t == data_atual]
+    if len(transacoes_hoje) > TRANSACOES_LIMITE:
+        print("Limite de transações diárias atingido. Tente novamente amanhã.")
+    else:
+        transacoes_realizadas.append(f"{natureza} - R$ {valor:.2f} - {data_atual.strftime('%d/%m/%Y %H:%M')}")
 
-def depositar(valor):
+def depositar(valor, natureza="Crédito"):
     global saldo, total_depositos, depositos_realizados
     while valor <= 0:
         print("-"*80)
@@ -19,10 +31,10 @@ def depositar(valor):
     saldo += valor
     total_depositos += valor
     depositos_realizados += 1
-    return saldo
+    registrar_transacao(valor, natureza)
+    return "Depósito efetuado com sucesso."
 
-
-def saque(valor):
+def saque(valor, natureza="Débito"):
     global saldo, saques_realizados, total_saques
     if saques_realizados > LIMITE_SAQUES:
         print("-"*80)
@@ -42,22 +54,25 @@ def saque(valor):
         saldo -= valor
         saques_realizados += 1
         total_saques += valor
+        registrar_transacao(valor, natureza)
         return "Saque efetuado com sucesso."
 
-
 def extrato():
-    print("--- EXTRATO BANCÁRIO ---")
-    print(f"DEPÓSITOS: R$ {total_depositos}")
-    print(f"SAQUES: R$ {total_saques}")
-    print(f"Saldo atual: R$ {saldo}")
+    print("---------- EXTRATO BANCÁRIO ----------")
+    for transacao in transacoes_realizadas:
+        print(transacao)
+    print("-"*80)
+    print(f"DEPÓSITOS: R$ {total_depositos:.2f}")
+    print(f"SAQUES: R$ {total_saques:.2f}")
+    print(f"Saldo atual: R$ {saldo:.2f}")
     print("-"*80)
     print(f"Total de saques realizados: {saques_realizados}")
     print(f"Total de depósitos realizados: {depositos_realizados}")
     print("-"*80)
 
-
 def menu():
     system = "Sistema Bancário"
+    print()
     print(f"{'>'*10} {system} {'<'*10}")
     print("""
     [0] - SAIR
@@ -68,28 +83,31 @@ def menu():
     print(f"{'>'*19}{'<'*19}")
     print()
 
-
 while True:
     menu()
-    opcao = input("Digite o número da opção desejada ->")
+    opcao = input("Digite o número da opção desejada -> ")
+    print()
 
     if opcao.isdigit():
         opcao = int(opcao)
         if opcao == 0:
+            print("-"*80)
             print("Obrigado por utilizar o sistema bancário")
+            print("-"*80)
+            print()
             break
         elif opcao == 1:
-            print(f"{'>'*10} [DEPÓSITO] {'<'*10}")
+            print(f"{'>'*14} [DEPÓSITO] {'<'*14}")
             print("-"*80)
             valor = float(
                 input("Digite o valor em dinheiro que desejas DEPOSITAR em sua conta -> "))
             print("-"*80)
-            depositar(valor)
             print("="*80)
-            print("| DEPÓSITO EFETUADO COM SUCESSO |")
+            mensagem_deposito = depositar(valor)
+            print(f"| {mensagem_deposito} |")
             print("="*80)
         elif opcao == 2:
-            print(f"{'>'*10} [SAQUE] {'<'*10}")
+            print(f"{'>'*14} [SAQUE] {'<'*14}")
             print("-"*80)
             valor = float(
                 input("Digite o valor em dinheiro que desejas SACAR em sua conta -> "))
@@ -99,7 +117,6 @@ while True:
             print(f"| {mensagem_saque} |")
             print("="*80)
         elif opcao == 3:
-            print(f"{'>'*10} [EXTRATO] {'<'*10}")
             print("-"*80)
             extrato()
             print("-"*80)
